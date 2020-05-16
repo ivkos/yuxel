8ca19f900a99d0608b194bed8e4c736a9126abfd
Ivaylo Stoyanov <me@ivkos.com>
2020-05-16 23:17:39 +0300

    Add endpoint for generating news by provider

--- src/api/NewsController.ts
+++ src/api/NewsController.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common"
+import { Controller, Get, Param } from "@nestjs/common"
 import { GeneratedNews } from "./dto/GeneratedNews"
 import { NewsProviderService } from "../news/providers/NewsProviderService"
 import { MarkovService } from "../news/scrape/MarkovService"
@@ -12,6 +12,19 @@ export class NewsController {
     async getNews(): Promise<GeneratedNews> {
         const titleBundle = await this.markovService.getBundle("title")
         const summaryBundle = await this.markovService.getBundle("title")
+
+        return {
+            title: titleBundle.markovski.generate().toUpperCase(),
+            summary: summaryBundle.markovski.generate().toUpperCase(),
+        }
+    }
+
+    @Get("/:providerId")
+    async getNewsFromProvider(@Param("providerId") providerId: string): Promise<GeneratedNews> {
+        const provider = await this.newsProviderService.findById(providerId)
+
+        const titleBundle = await this.markovService.getBundle("title", provider.id)
+        const summaryBundle = await this.markovService.getBundle("title", provider.id)
 
         return {
             title: titleBundle.markovski.generate().toUpperCase(),
